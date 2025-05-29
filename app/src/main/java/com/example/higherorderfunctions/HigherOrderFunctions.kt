fe4a0c1658aded412c3b6f1ce54c983410c02283
msai1 <medicherladurgasai@gamil.com>
2025-05-29 16:09:10 +0530

    Higher Order Functions Going to Give Commit

---
+++ app/src/main/java/com/example/higherorderfunctions/HigherOrderFunctions.kt
@@ -0,0 +1,10 @@
+package com.example.higherorderfunctions
+
+/*
+ * Higher Order Functions :
+   A Higher-Order Function is a function that either:
+   Takes another function as a parameter
+
+   Returns a function as its result
+   *
+ */
