765c2410b44cff6f1eed7beeb7aa4a9f45ba9737
msai1 <medicherladurgasai@gamil.com>
2025-06-02 05:53:49 +0530

    Higher Order Syntax and Simple Code Write

--- app/src/main/java/com/example/higherorderfunctions/HigherOrderFunctions.kt
+++ app/src/main/java/com/example/higherorderfunctions/HigherOrderFunctions.kt
@@ -1,10 +1,23 @@
 package com.example.higherorderfunctions
 
-/*
- * Higher Order Functions :
-   A Higher-Order Function is a function that either:
-   Takes another function as a parameter
 
-   Returns a function as its result
-   *
+/*
+ * Higher-Order Functions:
+ * A Higher-Order Function is a function that either:
+ * 👉 Takes another function as a parameter
+ * 👉 Returns a function as its result
  */
+
+
+fun higherOrderFunExample(a : Int, b : Int, operation : (Int, Int) -> Int){
+
+    var operation = operation(a,b)
+    print(operation)
+
+}
+
+fun main(){
+
+    higherOrderFunExample(a = 5,b = 5) { a,b -> a * b }
+
+}
